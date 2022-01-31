2a47094acfce23ec23e2aefb727d2f1e1acab502
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-01-31 17:14:52 -0600

    finish small exercise

--- src/App.js
+++ src/App.js
@@ -5,4 +5,33 @@ function App() {
 
 }
 
+//Small Exercise//
+class Person {
+  constructor(name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+  }
+  greet(other) {
+    console.log('Hello ' + other.name + ', I am ' + this.name + '!');
+  }
+}
+    
+let person1 = new Person('Sonny', 'sonny@hotmail.com', '483-485-4948');
+let person2 = new Person('Jordan', 'jordan@aol.com',  '495-586-3456');
+
+person1.greet(person2);
+person2.greet(person1);
+
+function printInfo(person) {
+  console.log(person.name + ': ' + person.email + ', ' + person.phone)
+}
+
+printInfo(person1);
+printInfo(person2);
+//End Small Exercise//
+
+
+
+
 export default App;
